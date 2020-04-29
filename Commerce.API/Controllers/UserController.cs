c29d121554cd96277dfcb55d75f9d73df5c6d4a7
Victor Popovich <victhor.popovich@gmail.com>
2020-04-28 21:28:26 -0300

    Create user controller

---
+++ Commerce.API/Controllers/UserController.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Commerce.Data.Models;
+using Commerce.Data.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Commerce.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class UserController : ControllerBase
+    {
+        private readonly ILogger<UserController> _logger;
+        private readonly IUserService _userService;
+
+        public UserController(ILogger<UserController> logger, IUserService userService)
+        {
+            _logger = logger;
+            _userService = userService;
+        }
+
+        public async Task<ActionResult<List<User>>> Get()
+        {
+            return await _userService.Get();
+        }
+
+        [HttpGet("{id:length(24)}", Name = "GetUser")]
+        public async Task<ActionResult<User>> Get(string id)
+        {
+            var user = await _userService.Get(id);
+
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            return user;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<User>> Create(User user)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    _logger.LogInformation("Creating new user named: {0}", user.Firstname);
+                    var createdUser = await _userService.Create(user);
+
+                    return CreatedAtRoute("GetUser", new { id = user.Id }, user);
+                }
+
+                return BadRequest("User did not pass model validation");
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "Error creating new user. User object: {0}", user);
+                return BadRequest(ex);
+            }
+        }
+    }
+}
