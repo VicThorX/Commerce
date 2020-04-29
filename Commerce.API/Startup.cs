77e965a4290c5eee0125a111e42efcf8a3c030cc
Victor Popovich <victhor.popovich@gmail.com>
2020-04-28 21:28:12 -0300

    Inject dependencies

--- Commerce.API/Startup.cs
+++ Commerce.API/Startup.cs
@@ -2,6 +2,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Commerce.Data.Configuration;
+using Commerce.Data.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -10,6 +12,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using MongoDB.Bson;
 
 namespace Commerce.API
 {
@@ -25,6 +28,10 @@ namespace Commerce.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.Configure<DatabaseConfiguration>(Configuration.GetSection(nameof(DatabaseConfiguration)));
+
+            services.AddScoped<IUserService, UserService>();
+
             services.AddControllers();
         }
 
