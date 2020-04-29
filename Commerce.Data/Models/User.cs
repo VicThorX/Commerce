85cc2eeb0cfa25be389fc148a147f39dcef5bf41
Victor Popovich <victhor.popovich@gmail.com>
2020-04-28 21:27:41 -0300

    Add user model

---
+++ Commerce.Data/Models/User.cs
@@ -0,0 +1,38 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Commerce.Data.Models
+{
+    public class User
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+
+        [BsonElement("Firstname")]
+        [Required]
+        public string Firstname { get; set; }
+
+        [BsonElement("Lastname")]
+        [Required]
+        public string Lastname { get; set; }
+
+        [BsonElement("EmailAddress")]
+        [Required]
+        public string EmailAddress { get; set; }
+
+        [BsonElement("Password")]
+        [Required]
+        public string Password { get; set; }
+
+        [BsonElement("HasPurchases")]
+        [Required]
+        public bool HasPurchases { get; set; }
+
+        [BsonElement("CreatedAt")]
+        [Required]
+        public DateTime CreatedAt { get; set; }
+    }
+}
