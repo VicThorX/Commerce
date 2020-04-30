0f7b42d01ced026e5f1286d15b789eb9bbe3eb70
Victor Popovich <victhor.popovich@gmail.com>
2020-04-30 12:05:15 -0300

    feat: add basic model prototype

---
+++ Commerce.Data/Models/Sale.cs
@@ -0,0 +1,12 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace Commerce.Data.Models
+{
+    public class Sale
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+    }
+}
