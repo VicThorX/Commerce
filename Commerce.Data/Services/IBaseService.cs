03cb2ea482935fb61416c267373594e98ac7a7e6
Victor Popovich <victhor.popovich@gmail.com>
2020-04-30 17:12:33 -0300

    refactor: create base service abstraction

---
+++ Commerce.Data/Services/IBaseService.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Commerce.Data.Services
+{
+    public interface IBaseService<T>
+    {
+        Task<T> Create(T category);
+        Task<T> Get(string id);
+        Task<List<T>> GetAll();
+        Task Remove(T categoryOut);
+        Task Remove(string id);
+        Task Update(string id, T categoryIn);
+    }
+}
