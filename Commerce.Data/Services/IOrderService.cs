29b004329081c18686a51e42ffc3a0f03fe73ec0
Victor Popovich <victhor.popovich@gmail.com>
2020-04-30 17:26:55 -0300

    feat: add order service

---
+++ Commerce.Data/Services/IOrderService.cs
@@ -0,0 +1,10 @@
+﻿using Commerce.Data.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Commerce.Data.Services
+{
+    public interface IOrderService : IBaseService<Order>
+    {
+    }
+}
---
+++ Commerce.Data/Services/OrderService.cs
@@ -0,0 +1,49 @@
+﻿using Commerce.Data.Models;
+using MongoDB.Driver;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Commerce.Data.Services
+{
+    public class OrderService : IOrderService
+    {
+        private readonly ICommerceContext _commerceContext;
+
+        public OrderService(ICommerceContext commerceContext)
+        {
+            _commerceContext = commerceContext;
+        }
+
+        public async Task<List<Order>> GetAll()
+        {
+            return await _commerceContext.Orders.Find(order => true).ToListAsync();
+        }
+
+        public async Task<Order> Get(string id)
+        {
+            return await _commerceContext.Orders.Find(order => order.Id == id).FirstOrDefaultAsync();
+        }
+
+        public async Task<Order> Create(Order order)
+        {
+            await _commerceContext.Orders.InsertOneAsync(order);
+
+            return order;
+        }
+
+        public async Task Update(string id, Order orderIn)
+        {
+            await _commerceContext.Orders.ReplaceOneAsync(order => order.Id == id, orderIn);
+        }
+
+        public async Task Remove(string id)
+        {
+            await _commerceContext.Orders.DeleteOneAsync(order => order.Id == id);
+        }
+
+        public async Task Remove(Order orderOut)
+        {
+            await _commerceContext.Orders.DeleteOneAsync(order => order.Id == orderOut.Id);
+        }
+    }
+}
