6bbe88aa4cee3c94a4682ddb92917f374ed7d1ee
Victor Popovich <victhor.popovich@gmail.com>
2020-04-30 19:51:36 -0300

    fix: name typo

--- Commerce.Data/Services/IBaseService.cs
+++ Commerce.Data/Services/IBaseService.cs
@@ -5,11 +5,11 @@ namespace Commerce.Data.Services
 {
     public interface IBaseService<T>
     {
-        Task<T> Create(T category);
+        Task<T> Create(T entity);
         Task<T> Get(string id);
         Task<List<T>> GetAll();
-        Task Remove(T categoryOut);
+        Task Remove(T entityOut);
         Task Remove(string id);
-        Task Update(string id, T categoryIn);
+        Task Update(string id, T entityIn);
     }
 }
